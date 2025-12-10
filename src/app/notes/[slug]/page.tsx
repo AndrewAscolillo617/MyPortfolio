cd3f772aab61995cc11b233def2ee155daad63f1
Andrew Ascolillo <ascolilloar@gmail.com>
2025-12-10 14:06:03 -0500

    fixed type mismatch in slug/page.tsx

--- src/app/notes/[slug]/page.tsx
+++ src/app/notes/[slug]/page.tsx
@@ -1,4 +1,6 @@
 // src/app/notes/[slug]/page.tsx
-export default function NotePage({ params }: { params: { slug: string } }) {
+
+
+export default async function NotePage({ params }: any ) {
   return <h1>Mock note: {params.slug}</h1>;
 }
